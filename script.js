40e56d70ef1ebcdd6a039d7054c64afd0f30a8f0
DavidG636 <gonzalezdavid636@gmail.com>
2019-07-19 20:32:55 -0400

    Add input box animation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $(function () {
   var backgroundArray = ['images/book.jpg', 'images/candle.jpg', 'images/darkMetal.jpg', 'images/stars.jpg', 'images/wood.jpg'];
-  $('body').css('background-image',`url(${backgroundArray[Math.floor(Math.random()*5)]}`);
+  $('.everything').css('background-image',`url(${backgroundArray[Math.floor(Math.random()*5)]}`);
 })
